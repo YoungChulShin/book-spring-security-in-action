85056e10c5a4de67ae2fb0d235f910402d071d70
YoungChulShin <go1323@gmail.com>
2023-07-16 20:24:47 +0900

    ch2-ex1: 초안

---
+++ ch2-ex1/src/test/java/study/spring/security/ch2ex1/Ch2Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch2ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
