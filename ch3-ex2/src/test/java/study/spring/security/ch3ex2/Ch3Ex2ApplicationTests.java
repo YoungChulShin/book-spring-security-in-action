81728c7a2f20000c219daf6f07b1f8ef55d03775
YoungChulShin <go1323@gmail.com>
2023-07-18 08:41:35 +0900

    ch3-ex2: 프로젝트 초안

---
+++ ch3-ex2/src/test/java/study/spring/security/ch3ex2/Ch3Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch3ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3Ex2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
