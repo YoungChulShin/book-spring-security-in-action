fe26ab5dcdc12df27c6110951a2da9aee85a2cc9
YoungChulShin <go1323@gmail.com>
2023-07-23 00:50:37 +0900

    ch5-ex4: formLogin 설정 구성

---
+++ ch5-ex4/src/main/java/study/spring/ch5ex4/config/ProjectConfig.java
@@ -0,0 +1,21 @@
+package study.spring.ch5ex4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .formLogin(Customizer.withDefaults())
+        .authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+    return http.build();
+  }
+
+}
