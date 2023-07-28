f0e88d42aa19c5035c3d460dbcd15700439664ee
YoungChulShin <go1323@gmail.com>
2023-07-28 21:36:42 +0900

    ch8-ex1: 프로젝트 초안

---
+++ ch8-ex1/src/main/kotlin/study/spring/security/ch8ex1/Ch8Ex1Application.kt
@@ -0,0 +1,11 @@
+package study.spring.security.ch8ex1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch8Ex1Application
+
+fun main(args: Array<String>) {
+	runApplication<Ch8Ex1Application>(*args)
+}
