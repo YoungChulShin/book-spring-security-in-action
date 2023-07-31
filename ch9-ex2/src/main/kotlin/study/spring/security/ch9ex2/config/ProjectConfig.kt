84fa91196690a44d4e3cf0fb4814ba5082718ac4
YoungChulShin <go1323@gmail.com>
2023-07-31 21:52:45 +0900

    ch9-ex2: filter 위치 설정

---
+++ ch9-ex2/src/main/kotlin/study/spring/security/ch9ex2/config/ProjectConfig.kt
@@ -0,0 +1,26 @@
+package study.spring.security.ch9ex2.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
+@Configuration
+class ProjectConfig(
+    private val staticKeyAuthenticationFilter: StaticKeyAuthenticationFilter,
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .addFilterAt(
+                staticKeyAuthenticationFilter,
+                BasicAuthenticationFilter::class.java)
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+
+        return http.build()
+    }
+}
