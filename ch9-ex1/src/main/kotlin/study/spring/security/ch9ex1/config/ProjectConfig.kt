c1cce3eac41ff1bc5a50d2931a1308fa0a96de77
YoungChulShin <go1323@gmail.com>
2023-07-31 17:55:15 +0900

    ch9-ex1: RequestValidationFilter를 BasicAuthenticationFilter전에 체크하도록 추가

---
+++ ch9-ex1/src/main/kotlin/study/spring/security/ch9ex1/config/ProjectConfig.kt
@@ -0,0 +1,22 @@
+package study.spring.security.ch9ex1.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+
+@Configuration
+class ProjectConfig {
+
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .addFilterBefore(
+                RequestValidationFilter(),
+                BasicAuthenticationFilter::class.java)
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+
+        return http.build()
+    }
+}
