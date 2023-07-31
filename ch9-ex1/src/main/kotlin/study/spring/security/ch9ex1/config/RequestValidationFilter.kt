1bb4e7f88b1171847e6729e9498f9ba040487d8e
YoungChulShin <go1323@gmail.com>
2023-07-31 17:54:43 +0900

    ch9-ex1: request-id를 check하는 filter 추가

---
+++ ch9-ex1/src/main/kotlin/study/spring/security/ch9ex1/config/RequestValidationFilter.kt
@@ -0,0 +1,30 @@
+package study.spring.security.ch9ex1.config
+
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+
+// jakarta.servlet.Filter
+class RequestValidationFilter: Filter {
+
+    override fun doFilter(
+        request: ServletRequest,
+        response: ServletResponse,
+        chain: FilterChain
+    ) {
+        val httpRequest = request as HttpServletRequest
+        val httpResponse = response as HttpServletResponse
+
+        val requestId = httpRequest.getHeader("Request-Id")
+
+        if (requestId.isBlank()) {
+            httpResponse.status = HttpServletResponse.SC_BAD_REQUEST
+            return
+        }
+
+        chain.doFilter(request, response)
+    }
+}
