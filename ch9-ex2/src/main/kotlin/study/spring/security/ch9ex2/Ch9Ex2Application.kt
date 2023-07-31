175a52378579c009bf43412e3ea28915b9a63034
YoungChulShin <go1323@gmail.com>
2023-07-31 21:44:16 +0900

    ch9-ex2: 프로젝트 초안

---
+++ ch9-ex2/src/main/kotlin/study/spring/security/ch9ex2/Ch9Ex2Application.kt
@@ -0,0 +1,11 @@
+package study.spring.security.ch9ex2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch9Ex2Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch9Ex2Application>(*args)
+}
---
+++ ch9-ex2/src/test/kotlin/study/spring/security/ch9ex2/Ch9Ex2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.security.ch9ex2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ch9Ex2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
