86e954d5a239531a96aee2b53c55c2907b6c7f7d
YoungChulShin <go1323@gmail.com>
2023-07-31 17:44:13 +0900

    ch9-ex1: 프로젝트 초안

---
+++ ch9-ex1/src/main/kotlin/study/spring/security/ch9ex1/Ch9Ex1Application.kt
@@ -0,0 +1,11 @@
+package study.spring.security.ch9ex1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch9Ex1Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch9Ex1Application>(*args)
+}
