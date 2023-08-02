c766b213ea5149c949d660f0dfbbfeaa6f21bcb4
YoungChulShin <go1323@gmail.com>
2023-08-03 02:26:54 +0900

    ch10-ex1: 프로젝트 초안

---
+++ ch10-ex1-csrf/src/main/java/study/spring/security/ch10ex1csrf/Ch10Ex1CsrfApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch10ex1csrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10Ex1CsrfApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch10Ex1CsrfApplication.class, args);
+  }
+
+}
---
+++ ch10-ex1-csrf/src/test/java/study/spring/security/ch10ex1csrf/Ch10Ex1CsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch10ex1csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch10Ex1CsrfApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
