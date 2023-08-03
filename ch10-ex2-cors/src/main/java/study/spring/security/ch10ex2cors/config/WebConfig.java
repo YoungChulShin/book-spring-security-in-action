323a4918f6f82464d389da9b5a435298d2b4bbcb
YoungChulShin <go1323@gmail.com>
2023-08-03 23:58:13 +0900

    ch10-ex2: cors 해결 #2 - SecurityFilterChain을 이용한 처리

--- ch10-ex2-cors/src/main/java/study/spring/security/ch10ex2cors/config/WebConfig.java
+++ ch10-ex2-cors/src/main/java/study/spring/security/ch10ex2cors/config/WebConfig.java
@@ -1,9 +1,12 @@
 package study.spring.security.ch10ex2cors.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class WebConfig {
@@ -12,6 +15,20 @@ public class WebConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
         .csrf(c -> c.disable())
+//        .cors(c -> {
+//          CorsConfigurationSource source = request -> {
+//            CorsConfiguration config = new CorsConfiguration();
+//            config.setAllowedOrigins(
+//                List.of("http://localhost:8080")
+//            );
+//            config.setAllowedMethods(
+//                List.of("POST")
+//            );
+//            return config;
+//          };
+//
+//          c.configurationSource(source);
+//        })
         .authorizeHttpRequests(c -> c.anyRequest().permitAll())
         .build();
   }
