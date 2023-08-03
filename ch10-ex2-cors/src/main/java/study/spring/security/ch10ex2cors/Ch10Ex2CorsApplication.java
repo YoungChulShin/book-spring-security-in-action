76c5dcb4c354eb389b589794395561057dbcd3a5
YoungChulShin <go1323@gmail.com>
2023-08-03 23:25:38 +0900

    ch10-ex2: 프로젝트 초안

---
+++ ch10-ex2-cors/src/main/java/study/spring/security/ch10ex2cors/Ch10Ex2CorsApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch10ex2cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch10Ex2CorsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch10Ex2CorsApplication.class, args);
+  }
+
+}
---
+++ ch10-ex2-cors/src/test/java/study/spring/security/ch10ex2cors/Ch10Ex2CorsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch10ex2cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch10Ex2CorsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
