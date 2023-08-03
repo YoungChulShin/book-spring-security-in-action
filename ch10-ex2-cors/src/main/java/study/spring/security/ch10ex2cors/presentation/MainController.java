2286da59d9e3041bebe8f4d84f6e5f0160aa3e8c
YoungChulShin <go1323@gmail.com>
2023-08-03 23:43:57 +0900

    ch10-ex2: cors 해결 #1 - CrossOrigin 애노테이션 이용

--- ch10-ex2-cors/src/main/java/study/spring/security/ch10ex2cors/presentation/MainController.java
+++ ch10-ex2-cors/src/main/java/study/spring/security/ch10ex2cors/presentation/MainController.java
@@ -3,6 +3,7 @@ package study.spring.security.ch10ex2cors.presentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,9 @@ public class MainController {
 
   @PostMapping("/test")
   @ResponseBody
+  // localhost:8080에 대한 교차 출처 요청을 허용한다.
+  // http 응답 헤더에 'Access-Control-Allow-Origin: http://localhost:8080' 가 추가된다.
+  // @CrossOrigin(value = "http://localhost:8080")
   public String test() {
     logger.info("test method called");
     return "HELLO";
