fc9f30f556a787a9711b0c5ad29ce31988479879
YoungChulShin <go1323@gmail.com>
2023-08-04 21:49:51 +0900

    ch11-ex1-business: test endpoint 추가

---
+++ ch11-ex1-login-project/business-server/src/main/java/study/spring/security/businessserver/adapter/in/web/TestController.java
@@ -0,0 +1,14 @@
+package study.spring.security.businessserver.adapter.in.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @GetMapping("/test")
+  public String test() {
+    return "Test";
+  }
+
+}
