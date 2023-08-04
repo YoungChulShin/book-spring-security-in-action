ece1c31c61a05901faf553d4e074d3b189e6fab6
YoungChulShin <go1323@gmail.com>
2023-08-04 10:56:20 +0900

    ch11-ex1-auth: 프로젝트 초안

---
+++ ch11-ex1-login-project/auth-server/src/main/java/study/spring/security/auth/Ch11Ex1AuthApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch11Ex1AuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch11Ex1AuthApplication.class, args);
+  }
+
+}
---
+++ ch11-ex1-login-project/auth-server/src/test/java/study/spring/security/auth/Ch11Ex1AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch11Ex1AuthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
