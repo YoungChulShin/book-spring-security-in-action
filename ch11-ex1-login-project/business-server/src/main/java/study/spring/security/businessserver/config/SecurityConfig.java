1d68095ee5b183e8063c4ccd93de1e3152f384cc
YoungChulShin <go1323@gmail.com>
2023-08-05 21:33:42 +0900

    ch11-ex1-auth: AuthenticationProvider가 2개있을 때 AuthenticationManager 빈 등록 코드 수정

--- ch11-ex1-login-project/business-server/src/main/java/study/spring/security/businessserver/config/SecurityConfig.java
+++ ch11-ex1-login-project/business-server/src/main/java/study/spring/security/businessserver/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package study.spring.security.businessserver.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class SecurityConfig {
 
+  // authenticationProvider가 1개 일 경우
+//  @Bean
+//  AuthenticationManager authenticationManager(
+//      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//    return authenticationConfiguration.getAuthenticationManager();
+//  }
+
+  // authenticationProvider가 2개 이상일 경우
   @Bean
-  AuthenticationManager authenticationManager(
-      AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
+  public AuthenticationManager authenticationManager(List<AuthenticationProvider> authenticationProviders) {
+    return new ProviderManager(authenticationProviders);
   }
 }
