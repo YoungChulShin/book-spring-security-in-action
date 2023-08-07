47b582f648d2874eef30edccc1d4b02f653b3e5f
YoungChulShin <go1323@gmail.com>
2023-08-08 05:34:34 +0900

    ch12-ex1-sso: 프로젝트 초안

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 # 코드 설명
 |폴더|설명|링크|
 |---|---|---|
-|ch7-ex1|권한, 역할 기반 접근 제어|[link](/ch7-ex1/)
+|ch7-ex1|권한, 역할 기반 접근 제어.|[link](/ch7-ex1/)
 |ch11-ex1-login-project|별도의 인증서버를 이용한 인증 기능 구현. MFA 지원.|[link](/ch11-ex1-login-project/)
-
+|ch12-ex1-sso|github을 권한부여 서버로 하는 SSO 구현.|[link](/ch12-ex1-sso/)
---
+++ ch12-ex1-sso/src/main/java/study/spring/security/ch12ex1sso/Ch12Ex1SsoApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch12ex1sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch12Ex1SsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch12Ex1SsoApplication.class, args);
+	}
+
+}
