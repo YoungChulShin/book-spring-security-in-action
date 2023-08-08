de5edc9616996035084e4c8b37c52de6726e7587
YoungChulShin <go1323@gmail.com>
2023-08-08 23:15:01 +0900

    ch12-ex1-sso: principal 로그 추가

--- ch12-ex1-sso/src/main/java/study/spring/security/ch12ex1sso/presentation/MainController.java
+++ ch12-ex1-sso/src/main/java/study/spring/security/ch12ex1sso/presentation/MainController.java
@@ -1,13 +1,19 @@
 package study.spring.security.ch12ex1sso.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+  private final Logger logger = LoggerFactory.getLogger(MainController.class);
+
   @GetMapping("/")
-  public String main() {
+  public String main(OAuth2AuthenticationToken token) {
+    logger.info(String.valueOf(token.getPrincipal()));
     return "main.html";
   }
 }
