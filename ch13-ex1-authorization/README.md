0d17289bbced459beed8cd763ce12b2ca5476080
YoungChulShin <go1323@gmail.com>
2023-08-09 23:02:47 +0900

    ch13-ex1: 프로젝트 초안

---
+++ ch13-ex1-authorization/README.md
@@ -0,0 +1,3 @@
+# 설명
+OAuth2 authorization 서버를 구현합니다. 
+
---
+++ ch13-ex1-authorization/src/main/java/study/spring/security/ch13ex1authorization/Ch13Ex1AuthorizationApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch13ex1authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch13Ex1AuthorizationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch13Ex1AuthorizationApplication.class, args);
+  }
+
+}
