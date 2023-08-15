f00781e08ad93966c158550eb21f0164d87d3c2e
YoungChulShin <go1323@gmail.com>
2023-08-16 07:14:33 +0900

    ch16-ex1: 파라미터를 가지는 엔드포인트 추가

--- ch16-ex1/src/main/java/study/spring/security/ch16ex1/application/NameService.java
+++ ch16-ex1/src/main/java/study/spring/security/ch16ex1/application/NameService.java
@@ -1,5 +1,7 @@
 package study.spring.security.ch16ex1.application;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,13 @@ public class NameService {
     return "Fantastico";
   }
 
+  public List<String> secretNames(String name) {
+    return secretNames.get(name);
+  }
+
+  private Map<String, List<String>> secretNames =
+      Map.of(
+          "ycshin", List.of("hello", "world"),
+          "mjseo", List.of("java", "kotlin")
+      );
 }
--- ch16-ex1/src/main/java/study/spring/security/ch16ex1/presentation/HelloController.java
+++ ch16-ex1/src/main/java/study/spring/security/ch16ex1/presentation/HelloController.java
@@ -1,6 +1,8 @@
 package study.spring.security.ch16ex1.presentation;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import study.spring.security.ch16ex1.application.NameService;
 
@@ -16,5 +18,12 @@ public class HelloController {
   @GetMapping("/hello")
   public String hello() {
     return "Hello, " + nameService.getName();
+  }
+
+  @GetMapping("/secret/names/{name}")
+  public List<String> names(
+      @PathVariable String name
+  ) {
+    return nameService.secretNames(name);
   }
 }
