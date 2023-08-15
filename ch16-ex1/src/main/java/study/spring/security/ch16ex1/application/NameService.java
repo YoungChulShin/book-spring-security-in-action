aa82d4406a6c300d302f40fa02b26c23856123ba
YoungChulShin <go1323@gmail.com>
2023-08-16 06:25:24 +0900

    ch16-ex1: 메서드에 사전 권한 부여 적용

--- ch16-ex1/src/main/java/study/spring/security/ch16ex1/application/NameService.java
+++ ch16-ex1/src/main/java/study/spring/security/ch16ex1/application/NameService.java
@@ -1,10 +1,12 @@
 package study.spring.security.ch16ex1.application;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameService {
 
+  @PreAuthorize("hasAuthority('write')")
   public String getName() {
     return "Fantastico";
   }
