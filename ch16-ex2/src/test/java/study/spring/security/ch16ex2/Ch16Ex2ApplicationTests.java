a959e412e452abf511bbcc1d762705cd6951e3ec
YoungChulShin <go1323@gmail.com>
2023-08-16 23:07:47 +0900

    ch16-ex2: 프로젝트 초안

---
+++ ch16-ex2/src/test/java/study/spring/security/ch16ex2/Ch16Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch16ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch16Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
