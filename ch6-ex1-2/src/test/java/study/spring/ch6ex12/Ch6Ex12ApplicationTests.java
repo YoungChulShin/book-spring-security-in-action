e7ba54750f5e7ac7c26c090de1f7f2b0e2b40da8
YoungChulShin <go1323@gmail.com>
2023-12-16 23:35:48 +0900

    ch6-ex1-2: 프로젝트 초안

---
+++ ch6-ex1-2/src/test/java/study/spring/ch6ex12/Ch6Ex12ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.ch6ex12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch6Ex12ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
