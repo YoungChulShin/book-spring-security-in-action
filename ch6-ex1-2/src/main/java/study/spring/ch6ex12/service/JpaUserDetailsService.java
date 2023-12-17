90deb560426e39507a2fb3bd54cc201333544bc8
YoungChulShin <go1323@gmail.com>
2023-12-17 10:32:29 +0900

    ch6-ex1-2: UserDetailsService 구현

---
+++ ch6-ex1-2/src/main/java/study/spring/ch6ex12/service/JpaUserDetailsService.java
@@ -0,0 +1,32 @@
+package study.spring.ch6ex12.service;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import study.spring.ch6ex12.domain.CustomUserDetails;
+import study.spring.ch6ex12.domain.User;
+import study.spring.ch6ex12.domain.UserRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public JpaUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Supplier<UsernameNotFoundException> s =
+        () -> new UsernameNotFoundException("Problem during authentication");
+
+    User u = userRepository.findUserByUsername(username)
+        .orElseThrow(s);
+
+    return new CustomUserDetails(u);
+  }
+}
