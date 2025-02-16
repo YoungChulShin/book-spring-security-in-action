d000cb0edfd46b2435c9c7e36106787bf2ec1147
YoungChulShin <go1323@gmail.com>
2025-02-16 18:27:59 +0900

    프로젝트 시작

---
+++ ch14-authorization-server/src/main/java/study/spring/security/ch14authorizationserver/Ch14AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch14authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch14AuthorizationServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch14AuthorizationServerApplication.class, args);
+  }
+
+}
---
+++ ch14-authorization-server/src/test/java/study/spring/security/ch14authorizationserver/Ch14AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch14authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch14AuthorizationServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
