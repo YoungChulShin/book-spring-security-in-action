4c4e30b39498ea61460e7050154039a8dca7fd62
YoungChulShin <go1323@gmail.com>
2025-02-21 06:34:10 +0900

    ch14 resource server - 프로젝트 추가

---
+++ ch14-resource-server/src/main/java/study/spring/security/ch14resourceserver/Ch14ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch14resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch14ResourceServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch14ResourceServerApplication.class, args);
+  }
+
+}
