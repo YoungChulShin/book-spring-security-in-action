dc9bfcedb2ee46f16f2e9641704905a0b30975c2
YoungChulShin <go1323@gmail.com>
2025-02-23 22:46:51 +0900

    ch15 client - 프로젝트 생성

--- ch14-resource-server/src/main/java/study/spring/security/ch14resourceserver/config/ProjectConfig.java
+++ ch14-resource-server/src/main/java/study/spring/security/ch14resourceserver/config/ProjectConfig.java
@@ -1,9 +1,19 @@
 package study.spring.security.ch14resourceserver.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -42,13 +52,45 @@ public class ProjectConfig {
 //            j.jwkSetUri(keySetUri);
 //            j.jwtAuthenticationConverter(converter);
 //          });
-          c.opaqueToken(o -> {
-            o
-                .introspectionUri(introspectionUri)
-                .introspectionClientCredentials(clientId, clientSecret);
-          });
+//          c.opaqueToken(o -> {
+//            o
+//                .introspectionUri(introspectionUri)
+//                .introspectionClientCredentials(clientId, clientSecret);
+//          });
+          c.authenticationManagerResolver(authenticationManagerResolver(jwtDecoder(), opaqueTokenIntrospector()));
         });
 
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver(
+      JwtDecoder jwtDecoder,
+      OpaqueTokenIntrospector opaqueTokenIntrospector) {
+    AuthenticationManager jwtAuth = new ProviderManager(new JwtAuthenticationProvider(jwtDecoder));
+    AuthenticationManager opaqueAuth = new ProviderManager(new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector));
+
+    return (request) -> {
+      if ("jwt".equals(request.getHeader("type"))) {
+        return jwtAuth;
+      } else {
+        return opaqueAuth;
+      }
+    };
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder
+        .withJwkSetUri("http://localhost:7070/oauth2/jwks")
+        .build();
+  }
+
+  @Bean
+  public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+    return new SpringOpaqueTokenIntrospector(
+        "http://localhost:6060/oauth2/introspect",
+        "clientId",
+        "secret");
   }
 }
---
+++ ch15-oauth2-client/src/main/java/study/spring/security/ch15oauth2client/Ch15Oauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package study.spring.security.ch15oauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch15Oauth2ClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ch15Oauth2ClientApplication.class, args);
+  }
+
+}
